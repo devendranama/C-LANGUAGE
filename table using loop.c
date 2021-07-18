67f6ac7e39b4c2863c59b4b3424dbfdb523be33d
DEVENDRA CHIPPI <84016500+devendranama@users.noreply.github.com>
2021-07-18 12:52:46 +0530

    Create table using loop.c

---
+++ table using loop.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+
+	 int num,i;
+	 printf("This program is made by Bharat patel\n");
+	  printf("enter your desired number \n");
+	  scanf("%d",&num);
+	 printf("The table of entered number is =\n");
+	 
+	 for(i=0; i<=10; i++)
+	 
+	  printf("\n%d*%d=%d",num,i,num*i);
+	
+			printf(" THANK YOU\n");
+
+			
+		return 0 ;
+	
+}
