5c886e1f04afab268d64d753a46f94a11870454d
DEVENDRA CHIPPI <84016500+devendranama@users.noreply.github.com>
2021-07-18 13:00:16 +0530

    Create if and else.c

---
+++ if and else.c
@@ -0,0 +1,21 @@
+  
+#include<stdio.h>
+int main()
+{
+	int a;
+	
+	printf("enter the number\n");
+	scanf("%d",&a);
+	
+	if ( a<10)
+	{
+		printf("correct statement\n");
+		
+	}
+	else
+{
+		printf("wrong sentence\n");
+	}
+	return 0;
+	
+}
