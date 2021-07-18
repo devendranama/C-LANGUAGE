2e012ce818b2fb78b1d8d13041dccfa9d6b61d25
DEVENDRA CHIPPI <84016500+devendranama@users.noreply.github.com>
2021-07-18 12:49:32 +0530

    Create use of if.c

---
+++ use of if.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	
+	printf("enter the number\n");
+	scanf("%d",&a);
+	
+	if ( a<10)
+	{
+		printf("correct statement\n");
+		
+	}
+	printf("wrong sentence\n");
+	return 0;
+	
+}
