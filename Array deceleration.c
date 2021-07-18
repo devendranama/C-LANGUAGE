0d3d1ba8a48057911071f7efef8c11c3942385b3
DEVENDRA CHIPPI <84016500+devendranama@users.noreply.github.com>
2021-07-18 13:15:15 +0530

    Create Array deceleration.c

---
+++ Array deceleration.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+
+ int m[5]={5,8,2,0,1};
+ printf("This is designed by Bharat Patel ");
+ printf("The numbers are %d \n",m[0]);
+ printf("The numbers are %d \n",m[1]);
+ printf("The numbers are %d \n",m[2]);
+ printf("The numbers are %d \n",m[3]);
+ printf("The numbers are %d \n",m[4]);
+	int i;
+	int num[]={5,9,6,3,7,4};
+	for(i=0; i<=5; i++)
+	{
+	  printf("\nThe numbers are %d \n",num[i]);	
+	}
+ int j; 
+ int nu[5];
+ for(j=0; j<=5; j++)
+ { 
+   
+   scanf(" %d \n",&nu[j]);
+ }
+	return 0;
+}
