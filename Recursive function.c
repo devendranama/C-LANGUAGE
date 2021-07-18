abae785737db977989438f76451f8ea01080f6a0
DEVENDRA CHIPPI <84016500+devendranama@users.noreply.github.com>
2021-07-18 13:06:25 +0530

    Create Recursive function.c

---
+++ Recursive function.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int rec(int);
+int main()
+{
+  int a,fact;
+  printf("This os designed by Bharat Patel");
+  printf("Enter any number :");
+  scanf("%d",&a);
+  
+  fact=rec(a);
+  printf("Factorial value=%d\n",fact);
+  return 0;
+}
+
+int rec(int x)
+{
+  int f;
+  if(x==1)
+  return (1);
+  
+  else
+  f=x*rec(x-1);
+ 
+  return (f);
+}
