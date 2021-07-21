dd4254c8f0e4ed9d0b251c0531edfebc6c698de5
DEVENDRA CHIPPI <84016500+devendranama@users.noreply.github.com>
2021-07-21 22:43:51 +0530

    Create call by reference.c

---
+++ call by reference.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void add( int *i, int *j );
+int main()
+{int i;
+  i=10;
+int j;
+ j= 20;
+ add(&i,&j);
+ printf("the curent value of i is %d\n",i);
+ return 0;
+ }
+
+void add( int *i, int *j )
+{
+	int k=*i+*j;
+	printf("the addition of two variables is %d \n",k);
+	*i=11;
+	*j=22;
+	k=*i+*j;
+	printf("the addition of two variables is %d \n",k);
+}
