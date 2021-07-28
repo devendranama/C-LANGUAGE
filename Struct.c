e8d46a5aa8f2a213a5d00b4ba798572e5199dfdd
DEVENDRA CHIPPI <84016500+devendranama@users.noreply.github.com>
2021-07-28 15:58:48 +0530

    Create Struct.c

---
+++ Struct.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+struct
+{
+ int roll_no;
+ int age;
+ char *section;
+}student1,student2,student3,student4,student5;
+ 
+int main()
+{
+ printf("Please enter roll no. , age and section of students \n");
+ 
+ scanf("%d",&student1.roll_no);
+ scanf("%d",&student1.age);
+ scanf("%c",&student1.section);
+ 
+ scanf("%d",&student2.roll_no);
+ scanf("%d",&student2.age);
+ scanf("%c",&student2.section);
+ 
+ scanf("%d",&student3.roll_no);
+ scanf("%d",&student3.age);
+ scanf("%c",&student3.section);
+ 
+ scanf("%d",&student4.roll_no);
+ scanf("%d",&student4.age);
+ scanf("%c",&student4.section);
+ 
+ scanf("%d",&student5.roll_no);
+ scanf("%d",&student5.age);
+ scanf("%c",&student5.section);
+ 
+ return 0;
+}
