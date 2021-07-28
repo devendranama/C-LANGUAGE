0cb4384fd68d49890a5cc28b3e2778d10d27b3df
DEVENDRA CHIPPI <84016500+devendranama@users.noreply.github.com>
2021-07-28 15:48:22 +0530

    Update Array deceleration.c

--- Array deceleration.c
+++ Array deceleration.c
@@ -3,7 +3,6 @@ int main()
 {
 
  int m[5]={5,8,2,0,1};
- printf("This is designed by Bharat Patel ");
  printf("The numbers are %d \n",m[0]);
  printf("The numbers are %d \n",m[1]);
  printf("The numbers are %d \n",m[2]);
