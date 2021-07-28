17493fa09c86348f5da5bcd8b4da60a05d5834fd
DEVENDRA CHIPPI <84016500+devendranama@users.noreply.github.com>
2021-07-28 15:55:54 +0530

    Update table using loop.c

--- table using loop.c
+++ table using loop.c
@@ -3,7 +3,6 @@ int main()
 {
 
 	 int num,i;
-	 printf("This program is made by Bharat patel\n");
 	  printf("enter your desired number \n");
 	  scanf("%d",&num);
 	 printf("The table of entered number is =\n");
